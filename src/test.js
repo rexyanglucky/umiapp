3543d1a95890ed5e7e493370bcece29fb0e32983
杨锦 <yangjin0@aixuexi.com>
2022-03-16 11:30:24 +0800

    update

--- src/test.js
+++ src/test.js
@@ -1,24 +1,23 @@
-// (function (root, factory) {
-//   // if (typeof exports === 'function') {
-//   //   exports = factory;
-//   // }
-//   if (typeof define === 'function' && define.amd) {
-//     define(factory);
-//   } else if (typeof exports === 'object') {
-//     module.exports = factory();
-//   } else {
-//     window.test = factory();
-//   }
-// })(this, function () {
-//   return {
-//     said() {
-//       console.log('hhhh');
-//     }
-//   }
-// })
-module.exports = {
-  said() {
-    debugger
-    console.log('hhhh');
+(function (root, factory) {
+  // if (typeof exports === 'function') {
+  //   exports = factory;
+  // }
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    window.test = factory();
   }
-}
+})(this, function () {
+  return {
+    said() {
+      console.log('hhhh');
+    }
+  }
+})
+// module.exports = {
+//   said() {
+//     console.log('hhhh');
+//   }
+// }
